dcc6290560aaabb071878b684f2024e46ba83274
johnsnow09 <48548048+johnsnow09@users.noreply.github.com>
2024-07-25 13:25:12 +0530

    Update Health_data_app_streamlit.py

--- Health_data_app_streamlit.py
+++ Health_data_app_streamlit.py
@@ -507,7 +507,9 @@ st.write("\n1. **Thyroid - TSH** is continuously **high** except in 2 instance c
          \n6. **Cholride** shows **correlation** with **lymphocytes** (correlation research report proof https://pubmed.ncbi.nlm.nih.gov/7687269/).    \n\
          \n7. **TSH** shows very high +ve correlation with **Hba1c** and -ve correlation with **Bilrubin Indirect** which indicates it can lead to or already leading to    \
          **Diabetes** and `low` **Bilrubin Production** and it shows in `increasing` **hba1c** over time and `falling` **Bilrubin indirect** but `abnormal increase` in **Bilrubin direct** indicating **liver** damage.    \
-         This can lead to **stones** in the **Gall Bladder** in Future    \n\
-         \n8. `Low Levels` of **Potassium** can be causing issues of **Insulin Resistance** as **Low Potassium** leads to `low` **Insulin Production** (as per this article: https://www.webmd.com/diabetes/potassium-diabetes)")
+         This can lead to **stones** in the **Gall Bladder** in Future.    \n\
+         \n8. `Low Levels` of **Potassium** can be causing issues of **Insulin Resistance** as **Low Potassium** leads to `low` **Insulin Production** (as per this article: https://www.webmd.com/diabetes/potassium-diabetes).    \n\
+         \n8. `Low Levels` of **Potassium & Sodium** recently indicates **dehydration** which can be the reason of weight loss. This shows electrolytes imbalance and also has `low` **Calcium**. All of this indicating possible    \
+         `low levels` of **Magnesium** in the body too.")
 
 ############################## MY INSIGHTS / OBSERVATIONS FROM DATA ############################## 
