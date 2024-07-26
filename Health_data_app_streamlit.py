572d7a1d4cd3f063c001e734ba81d159d5e44ad3
johnsnow09 <48548048+johnsnow09@users.noreply.github.com>
2024-07-26 18:23:06 +0530

    Update Health_data_app_streamlit.py

--- Health_data_app_streamlit.py
+++ Health_data_app_streamlit.py
@@ -148,8 +148,8 @@ df_test_mapping = df_test_mapping.with_columns(
                .then(pl.lit('Anemia Profile'))
                .when(pl.col('test').is_in(["VOLUME","pH","SPECIFIC GRAVITY","PUS CELLS"]))
                .then(pl.lit('Urine Examination'))
-               .otherwise(pl.lit('Others')).alias('test_category')  
-) # "GLUCOSE IN URINE",
+               .otherwise(pl.lit('Others')).alias('test_category')  # "GLUCOSE IN URINE",
+) 
 
 ############################## CREATING TEST DATA MAPPING ##############################
 
